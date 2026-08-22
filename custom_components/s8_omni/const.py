3e80dce9e642363460b65f9aa49479644d394597
Nikolay <110602332+NikaSir@users.noreply.github.com>
2026-08-22 15:10:03 +0300

    release: bump S8 OMNI to b013 / UI v0.5.1

--- custom_components/s8_omni/const.py
+++ custom_components/s8_omni/const.py
@@ -1,6 +1,6 @@
 DOMAIN = "s8_omni"
-VERSION = "v1.00_b012"
-DASHBOARD_VERSION = "v0.5.0"
+VERSION = "v1.00_b013"
+DASHBOARD_VERSION = "v0.5.1"
 
 PANEL_ID = "s8_omni"
 PANEL_PATH = "dashboard-s8-omni"
