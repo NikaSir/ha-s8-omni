575bfc10cc55550dfd48e832c77607738a343724
Nikolay <110602332+NikaSir@users.noreply.github.com>
2026-08-22 09:48:08 +0300

    chore: bump S8 OMNI panel build to b005

--- custom_components/s8_omni/const.py
+++ custom_components/s8_omni/const.py
@@ -1,6 +1,6 @@
 DOMAIN = "s8_omni"
-VERSION = "v1.00_b004"
-DASHBOARD_VERSION = "v0.2.0"
+VERSION = "v1.00_b005"
+DASHBOARD_VERSION = "v0.2.1"
 
 PANEL_ID = "s8_omni"
 PANEL_PATH = "dashboard-s8-omni"
