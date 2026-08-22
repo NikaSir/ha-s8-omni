2e1cbe638dac6f4b51924bccdb242d3ae384bcaa
Nikolay <110602332+NikaSir@users.noreply.github.com>
2026-08-22 18:25:35 +0300

    chore: bump S8 OMNI to v1.00_b015

--- custom_components/s8_omni/const.py
+++ custom_components/s8_omni/const.py
@@ -1,6 +1,6 @@
 DOMAIN = "s8_omni"
-VERSION = "v1.00_b014"
-DASHBOARD_VERSION = "v0.5.2"
+VERSION = "v1.00_b015"
+DASHBOARD_VERSION = "v0.5.3"
 
 PANEL_ID = "s8_omni"
 PANEL_PATH = "dashboard-s8-omni"
